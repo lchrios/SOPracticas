f2484ca05f49ded3ea5fb81c6832122e8502b1cb
Christopher Ortega <A01634500@itesm.mx>
2020-04-22 00:16:54 +0000

    practica7correccion

--- practica8/chrome.c
+++ practica8/chrome.c
@@ -7,7 +7,7 @@
 #include <sys/wait.h>
 
 int main(){
-	int shmId = shm_get(27,100,0644 | IPC_CREAT);
+	int shmId = shm_get(200 , 400, 0644 | IPC_CREAT);
 	/*
 	char* var;
 	var = (char *)shmat(shmId,NULL,0);
@@ -21,8 +21,8 @@ int main(){
 		int pid = fork();
 		hijos[i] = pid;
 		if (pid != 0) {
-			shmId = shmget(200,300,0644);
-			char * var = (char *)shmat(shmId,NULL,0);
+			shmId = shmget(200, 400, 0644);
+			int *var = (int *)shmat(shmId,NULL,0);
 			var = &var[i*10];
 			for(int j=0; j<10; j++){
 				var[j] = i + '0';
@@ -33,7 +33,11 @@ int main(){
 	for(int i=0; i<5;i++){
 		waitpid(hijos[i],0,0);
 	}
-	char *var = (char *)shmat(shmId,NULL,0);
-	printf("El contenido es %s",var);
+	int *var = (int *)shmat(shmId,NULL,0);
+    for (int i=0; i<100;i++){
+        printf("%d,",var);
+    }
+    printf("\n");
+
 	return 0;
 }
